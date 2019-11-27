df638bf1fb1101314e45a554e295b2e14a992411
Joshua-McMillin <58111885+Joshua-McMillin@users.noreply.github.com>
2019-11-27 00:36:09 -0700

    Create Station.m
    Class definition file for the Station data.

---
+++ Station.m
@@ -0,0 +1,12 @@
+classdef Station
+    properties
+        Mach;
+        Velocity_ms;
+        Pressure_Pa;
+        TotalPressure_Pa;
+        Temperature_K;
+        TotalTemperature_K;
+        MassFlowRate_kgs;
+        Area_m2;
+    end
+end
